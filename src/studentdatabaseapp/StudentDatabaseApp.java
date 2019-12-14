61a775a9b7725c1303181a921a5c42d1a1b77436
Arjun T <creatives.arjun@gmail.com>
2019-12-14 22:05:39 +0530

    Initialize Student Constructor

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,13 +1,19 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args){
 
+        Student stu1 = new Student();
+
+
         //Ask how many users we want to add
 
         //Create n number of new students
+        }
 
 
     }
-}
+
