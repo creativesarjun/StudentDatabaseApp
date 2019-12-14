c0a8a4dd797fe5da2de756011b21d4542687bd52
Arjun T <creatives.arjun@gmail.com>
2019-12-14 20:24:56 +0530

    Define the Outline for working the code

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,19 @@
 package studentdatabaseapp;
 
 public class Student {
+
+
+    // Constructor prompt user to enter name & year
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay Tuition
+
+    // Show status
+
 }
+
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,6 +4,10 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args){
 
+        //Ask how many users we want to add
+
+        //Create n number of new students
+
 
     }
 }
